d29ed763b7789cc934867911b6ed10769ab14749
Ulpio <ulpionetto0@gmail.com>
2025-08-12 15:31:44 -0300

    feat: :sparkles: adicionar função InsertAdmin para criação de administrador

--- main.go
+++ main.go
@@ -1,13 +1,25 @@
 package main
 
 import (
+	"fmt"
+
 	"github.com/Ulpio/reservas-cipt/config"
 	"github.com/Ulpio/reservas-cipt/database"
 	"github.com/Ulpio/reservas-cipt/routes"
+	"github.com/Ulpio/reservas-cipt/services"
 )
 
 func main() {
 	config.LoadEnv()
 	database.ConnectDB()
+	InsertAdmin()
 	routes.SetupRoutes()
+}
+
+func InsertAdmin() {
+	_, err := services.CreateAdmin("Ulpio Paulo de Miranda Netto", "13366671416")
+	if err != nil {
+		return
+	}
+	fmt.Println("Admin criado com sucesso")
 }
