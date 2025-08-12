647e24d09b726b5c039b52ae1bf9e9b2b38b3d95
Ulpio <ulpionetto0@gmail.com>
2025-08-12 15:32:10 -0300

    feat: :sparkles: adicionar rotas de usuário com autenticação

---
+++ routes/user_routes.go
@@ -0,0 +1,16 @@
+package routes
+
+import (
+	"github.com/Ulpio/reservas-cipt/handlers"
+	"github.com/Ulpio/reservas-cipt/middleware"
+	"github.com/gin-gonic/gin"
+)
+
+func SetupUserRoutes(r *gin.Engine) {
+	users := r.Group("/user")
+	users.GET("/", middleware.JWTAuthMiddleware(), handlers.GetAllUsersHandler)
+	users.GET("/me", middleware.JWTAuthMiddleware(), handlers.MeHandler)
+	users.POST("/", middleware.JWTAuthMiddleware(), handlers.CreateUserHandler)
+	users.GET("/:id", handlers.GetUserByIDHandler)
+	users.DELETE("/:id", middleware.JWTAuthMiddleware(), handlers.DeleteUserHandler)
+}
