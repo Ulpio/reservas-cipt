d91d1db463ada8a76433df2927d60f531cc15393
Ulpio <ulpionetto0@gmail.com>
2025-08-11 23:48:26 -0300

    feat: :sparkles: conexão e migration

---
+++ database/postgres.go
@@ -0,0 +1,38 @@
+package database
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/Ulpio/reservas-cipt/models"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+var DB *gorm.DB
+
+func ConnectDB() {
+	host := os.Getenv("DB_HOST")
+	port := os.Getenv("DB_PORT")
+	user := os.Getenv("DB_USER")
+	password := os.Getenv("DB_PASSWORD")
+	dbname := os.Getenv("DB_NAME")
+	sslmode := os.Getenv("DB_SSLMODE") // Ex: disable
+
+	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=%s",
+		host, user, password, dbname, port, sslmode,
+	)
+
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		log.Fatalf("Erro ao conectar ao banco de dados: %v", err)
+	}
+
+	DB = db
+	MigrateModels()
+}
+
+func MigrateModels() {
+	DB.AutoMigrate(&models.User{})
+}
