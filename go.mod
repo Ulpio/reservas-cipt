eabd20147d587e7bc9e3e7c1be997cd1ade05799
Ulpio <ulpionetto0@gmail.com>
2025-09-03 14:00:39 -0300

    all origins cors

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/Ulpio/reservas-cipt
 go 1.24.6
 
 require (
+	github.com/gin-contrib/cors v1.7.6
 	github.com/gin-gonic/gin v1.10.1
 	github.com/golang-jwt/jwt/v5 v5.3.0
 	github.com/joho/godotenv v1.5.1
--- routes/routes.go
+++ routes/routes.go
@@ -2,6 +2,7 @@ package routes
 
 import (
 	_ "github.com/Ulpio/reservas-cipt/docs"
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 	swaggerFiles "github.com/swaggo/files"
 	ginSwagger "github.com/swaggo/gin-swagger"
@@ -9,7 +10,11 @@ import (
 
 func SetupRoutes() {
 	r := gin.Default()
-
+	r.Use(cors.New(cors.Config{
+		AllowAllOrigins: true,
+		AllowMethods:    []string{"GET", "POST", "PATCH", "PUT", "DELETE", "OPTIONS"},
+		AllowHeaders:    []string{"Origin", "Content-Type", "Authorization"},
+	}))
 	r.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
 	api := r.Group("/api/v1")
 	SetupAuthRoutes(api)
